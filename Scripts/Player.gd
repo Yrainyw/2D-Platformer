8366a4093bdaf0794e1003bfab502b5ad0b3cf70
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-06-30 14:07:58 +0800

    Coyote Time

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -24,7 +24,7 @@ func _process(delta):
 		
 	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 
-	if (moveVector.y < 0 && is_on_floor()):
+	if (moveVector.y < 0 && (is_on_floor() || !$CoyoteTimer.is_stopped())):
 		velocity.y = moveVector.y * jumpSpeed
 		
 	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
@@ -32,7 +32,11 @@ func _process(delta):
 	else:
 		velocity.y += gravity * delta
 		
+	var wasOnFloor = is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if (wasOnFloor && !is_on_floor()):
+		$CoyoteTimer.start()
 	
 	updateAnimation()
 	
