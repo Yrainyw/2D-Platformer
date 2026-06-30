2a4b3d670b6585702c25601fad82df2be58d3c9d
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-06-30 14:55:19 +0800

    Adding Collectables
    Also fixed camera lag

--- Scripts/GameCamera.gd
+++ Scripts/GameCamera.gd
@@ -1,20 +1,19 @@
 extends Camera2D
 
-var targetPosition = Vector2.ZERO
-
 export(Color, RGB) var backgroundColor
+export(float, 0.0, 0.999) var smoothing = 0.0001 # 数值越小,跟随越紧;越接近1,跟随越松,拖尾感越强
 
+var targetPosition := Vector2.ZERO
 
 func _ready():
 	VisualServer.set_default_clear_color(backgroundColor)
 
 func _process(delta):
 	acquire_target_position()
-	global_position = lerp(targetPosition, global_position, pow(2, -15) * delta)
-
+	var weight = 1.0 - pow(smoothing, delta)
+	global_position = lerp(global_position, targetPosition, weight)
 
 func acquire_target_position():
 	var players = get_tree().get_nodes_in_group("player")
-	if (players.size() > 0):
-		var player = players[0]
-		targetPosition = player.global_position
+	if players.size() > 0:
+		targetPosition = players[0].global_position
