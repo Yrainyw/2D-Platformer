116e92bab412e2f7e5d05421c3ef4e18e1bec737
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-07-01 15:22:10 +0800

    Making a Test Level

--- Scripts/GameCamera.gd
+++ Scripts/GameCamera.gd
@@ -2,7 +2,7 @@
 extends Camera2D
 
 export(Color, RGB) var backgroundColor
-export(float, 0.0, 0.999) var smoothing = 0.0001
+export(float, 0.0, 1.0) var weight = 0.15
 
 var targetPosition := Vector2.ZERO
 var targetNode = null  # 改成持有具体引用,而不是每帧搜索分组
@@ -13,7 +13,6 @@ func _ready():
 func _process(delta):
 	if targetNode != null and is_instance_valid(targetNode):
 		targetPosition = targetNode.global_position
-	var weight = 1.0 - pow(smoothing, delta)
 	global_position = lerp(global_position, targetPosition, weight)
 
 func set_target(node):
