975e2f679008b7fc4d2a614a1da234b6ef322d8a
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-07-02 11:37:16 +0800

    Changing Levels

--- Scripts/baseLevel.gd
+++ Scripts/baseLevel.gd
@@ -16,6 +16,8 @@ func _ready():
 	register_player($Player)
 	coin_total_changed(get_tree().get_nodes_in_group("coin").size())
 	
+	$Flag.connect("player_won", self, "on_player_won")
+	
 	
 func coin_collected():
 	collectedCoins += 1
@@ -43,3 +45,7 @@ func create_player():
 func on_player_died():
 	currentPlayerNode.queue_free()
 	create_player()
+
+
+func on_player_won():
+	$"/root/LevelManager".increment_level()
