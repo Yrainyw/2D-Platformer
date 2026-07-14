eac69d30c9afed1b4e88496331a0faf35bb48402
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-07-14 17:37:44 +0800

    UI and Sound Fixes

--- Scripts/animatedButton.gd
+++ Scripts/animatedButton.gd
@@ -5,6 +5,7 @@ export(bool) var disableHoverAnim
 func _ready():
 	connect("mouse_entered", self, "on_mouse_entered")
 	connect("mouse_exited", self, "on_mouse_exited")
+	connect("focus_exited", self, "on_focus_exited")
 	connect("pressed", self, "on_pressed")
 	
 	
@@ -12,16 +13,23 @@ func _process(_delta):
 	rect_pivot_offset = rect_min_size / 2
 
 
+func reset_button_state():
+	if (!disableHoverAnim):
+		$HoverAnimationPlayer.play_backwards("hover")
+
 func on_mouse_entered():
 	if (!disableHoverAnim):
 		$HoverAnimationPlayer.play("hover")
 	
 
 func on_mouse_exited():
-	if (!disableHoverAnim):
-		$HoverAnimationPlayer.play_backwards("hover")
+	reset_button_state();
 
 
 func on_pressed():
 	$ClickAnimationPlayer.play("click")
 	$AudioStreamPlayer.play()
+
+
+func on_focus_exited():
+	reset_button_state()
--- Scripts/screenTransitionManager.gd
+++ Scripts/screenTransitionManager.gd
@@ -4,6 +4,10 @@ var screenTransitionScene = preload("res://Scenes/UI/screenTransition.tscn")
 
 
 func transition_to_scene(scenePath):
+	for button in get_tree().get_nodes_in_group("animated_button"):
+		button.disabled = true
+		
+	yield(get_tree().create_timer(.1), "timeout")
 	var screenTransition = screenTransitionScene.instance()
 	add_child(screenTransition)
 	yield(screenTransition, "screen_covered")
