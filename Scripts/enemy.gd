5418b0a10db832107ae8ba006056738b7b099cd1
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-07-15 10:31:09 +0800

    Fixed Enemy Spawning

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,50 +1,54 @@
 extends KinematicBody2D
 
 var enemyDeathScene = preload("res://Scenes/Enemy/EnemyDeath.tscn")
-
 var maxSpeed = 25
 var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
 var gravity = 500
 var startDirection = Vector2.RIGHT
-
 export var isSpawning = true
 
+onready var animationPlayer = $AnimationPlayer
 
 func _ready():
 	direction = startDirection
 	$GoalDetector.connect("area_entered", self, "on_goal_entered")
 	$HitboxArea.connect("area_entered", self, "on_hitbox_entered")
+	
+	animationPlayer.connect("animation_finished", self, "on_spawn_animation_finished")
+	animationPlayer.play("spawn")
 
 
-func _process(delta):
-	if (isSpawning):
+func _physics_process(delta):
+	if isSpawning:
+		velocity = Vector2.ZERO
 		return
 	
 	velocity.x = (direction * maxSpeed).x
-	
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
-	$Visuals/AnimatedSprite.flip_h = true if direction .x > 0 else false
+	$Visuals/AnimatedSprite.flip_h = true if direction.x > 0 else false
 
+
+func on_spawn_animation_finished(anim_name):
+	if anim_name == "spawn":
+		isSpawning = false
+		$Visuals/AnimatedSprite.play("walk")
 
 func kill():
 	var deathInstance = enemyDeathScene.instance()
 	get_parent().add_child(deathInstance)
 	deathInstance.global_position = global_position
 	
-	if (velocity.x > 0):
+	if velocity.x > 0:
 		deathInstance.scale = Vector2(-1, 1)
-		
+	
 	queue_free()
-
 
 func on_goal_entered(_area2d):
 	direction *= -1
-	
-	
+
 func on_hitbox_entered(_area2d):
 	$"/root/Helpers".apply_camera_shake(1)
 	call_deferred("kill")
-	
